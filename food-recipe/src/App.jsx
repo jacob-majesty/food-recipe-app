41e6027d4aa3581cc3b0969f50ae1cac8314e4b8
jacob-majesty <jacobamony@gmail.com>
2025-01-01 13:01:23 -0300

    feat: Add pictures to public

--- food-recipe/src/App.jsx
+++ food-recipe/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import Details from './pages/Details'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
