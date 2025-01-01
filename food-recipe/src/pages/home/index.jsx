cb54ad1e60beea2cde34a828466512151ee17484
jacob-majesty <jacobamony@gmail.com>
2025-01-01 13:21:43 -0300

    feat: add files in Page

--- food-recipe/src/pages/home/index.jsx
+++ food-recipe/src/pages/home/index.jsx
@@ -1,9 +1,23 @@
-
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
 export default function Home() {
-    return (
+  const { recipeList, loading } = useContext(GlobalContext);
+
+  if (loading) return <div>Loading...Please wait!</div>;
+
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
         <div>
-            <h1>Home</h1>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
+          </p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
